ac9b3d1015a9134911e032f080bfde0bf4547c0b
thanchanok <std.67122420226@ubru.ac.th>
2025-12-08 11:04:11 +0700

    edit more file

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return '''
+
   <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,5 +19,25 @@ def index():
 </html>
   '''
 
-  if __name__ == '__main__':
-    app.run(debug=True)
+  @app.route('/<name>')
+  def show_name(name):
+    return f'<h1>My name is {name}</h1>'
+
+  @app.route('/hello/<name>')
+  def hello(name):
+    return f'<h1>Hello, {name}</h1>'
+
+  @app.route('/greeting/<name>/<age>')
+  def greeting(name, age):
+    return f'<h1>My name is {name}. I am {age} years old.</h1>'
+
+  @app.route('/calculate/addition/<int:a>/<int:b>')
+  def subtraction(a, b):
+    return f'<h1>{a} + {b} = {a+b}</h1>'
+
+  @app.route('/secrekey/<uuid:sk>')
+  def secret_key(sk):
+    return f'my secret kay : {sk}'
+
+#  if __name__ == '__main__':
+#    app.run(debug=True)
